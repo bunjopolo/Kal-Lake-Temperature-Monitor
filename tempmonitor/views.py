b2cf0e062bf0672e31289b81faccc596664b890c
bunjopolo <76890407+bunjopolo@users.noreply.github.com>
2023-09-10 18:42:06 -0700

    modified:   tempmonitor/views.py

--- tempmonitor/views.py
+++ tempmonitor/views.py
@@ -22,28 +22,57 @@ def webhook_handler(request):
 
 
 
+# def temperature_data(request):
+#     temperatures = [float(reading.temp) for reading in temperatureReading.objects.all()]
+#     dates = [reading.time.strftime("%d/%m/%Y") for reading in temperatureReading.objects.all()]
+
+#     try: 
+#         avg = sum(temperatures)/len(temperatures)
+#         avg = round(avg, 2)
+
+#         monthly_avg = [] #list of average temperatures for each month
+
+#         #get average temperature for each month
+#         for month in range(1, 13):
+#             monthly_temperatures = [float(reading.temp) for reading in temperatureReading.objects.filter(time__month=month)]
+#             if len(monthly_temperatures) > 0:
+#                 monthly_avg.append(round(sum(monthly_temperatures)/len(monthly_temperatures), 2))
+#             else:
+#                 monthly_avg.append(0)
+#     except:
+#         avg = 0
+#         monthly_avg = [0] * 12
+
+#     return JsonResponse({'temperatures': temperatures, 'dates': dates, 'avg': avg, 'monthly_avg': monthly_avg})
+
+from django.db.models import Avg
+from django.db.models.functions import TruncDate
+from django.http import JsonResponse
+
 def temperature_data(request):
-    temperatures = [float(reading.temp) for reading in temperatureReading.objects.all()]
-    dates = [reading.time.strftime("%d/%m/%Y") for reading in temperatureReading.objects.all()]
+    # Group readings by date and calculate the daily average temperature
+    daily_avg_queryset = temperatureReading.objects.annotate(date=TruncDate('time')).values('date').annotate(avg_temp=Avg('temp')).order_by('date')
+    
+    # Extract the dates and daily average temperatures from the queryset
+    dates = [entry['date'].strftime("%d/%m/%Y") for entry in daily_avg_queryset]
+    daily_avg_temperatures = [entry['avg_temp'] for entry in daily_avg_queryset]
 
-    try: 
-        avg = sum(temperatures)/len(temperatures)
-        avg = round(avg, 2)
+    # Calculate the overall average temperature
+    overall_avg = round(sum(daily_avg_temperatures) / len(daily_avg_temperatures), 2)
 
-        monthly_avg = [] #list of average temperatures for each month
+    # Calculate the monthly average temperatures
+    monthly_avg = []
+    for month in range(1, 13):
+        monthly_temperatures = [entry['avg_temp'] for entry in daily_avg_queryset if entry['date'].month == month]
+        if monthly_temperatures:
+            monthly_avg.append(round(sum(monthly_temperatures) / len(monthly_temperatures), 2))
+        else:
+            monthly_avg.append(0)
 
-        #get average temperature for each month
-        for month in range(1, 13):
-            monthly_temperatures = [float(reading.temp) for reading in temperatureReading.objects.filter(time__month=month)]
-            if len(monthly_temperatures) > 0:
-                monthly_avg.append(round(sum(monthly_temperatures)/len(monthly_temperatures), 2))
-            else:
-                monthly_avg.append(0)
-    except:
-        avg = 0
-        monthly_avg = [0] * 12
+    return JsonResponse({'dates': dates, 'daily_avg_temperatures': daily_avg_temperatures, 'overall_avg': overall_avg, 'monthly_avg': monthly_avg})
 
-    return JsonResponse({'temperatures': temperatures, 'dates': dates, 'avg': avg, 'monthly_avg': monthly_avg})
+
+
     
 @csrf_exempt
 def home(request):
