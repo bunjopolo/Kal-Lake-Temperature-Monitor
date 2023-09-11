cbc56ff888d79c996bce538ea3c350ce2d5a1eb1
bunjopolo <76890407+bunjopolo@users.noreply.github.com>
2023-09-10 19:55:56 -0700

    modified:   tempmonitor/views.py

--- tempmonitor/views.py
+++ tempmonitor/views.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.db.transaction import non_atomic_requests, atomic
 from django.views.decorators.http import require_POST
 from .models import temperatureReading
+from django.db.models import Avg
+from django.db.models.functions import TruncDate
 
 
 @csrf_exempt
@@ -22,32 +24,7 @@ def webhook_handler(request):
 
 
 
-# def temperature_data(request):
-#     temperatures = [float(reading.temp) for reading in temperatureReading.objects.all()]
-#     dates = [reading.time.strftime("%d/%m/%Y") for reading in temperatureReading.objects.all()]
 
-#     try: 
-#         avg = sum(temperatures)/len(temperatures)
-#         avg = round(avg, 2)
-
-#         monthly_avg = [] #list of average temperatures for each month
-
-#         #get average temperature for each month
-#         for month in range(1, 13):
-#             monthly_temperatures = [float(reading.temp) for reading in temperatureReading.objects.filter(time__month=month)]
-#             if len(monthly_temperatures) > 0:
-#                 monthly_avg.append(round(sum(monthly_temperatures)/len(monthly_temperatures), 2))
-#             else:
-#                 monthly_avg.append(0)
-#     except:
-#         avg = 0
-#         monthly_avg = [0] * 12
-
-#     return JsonResponse({'temperatures': temperatures, 'dates': dates, 'avg': avg, 'monthly_avg': monthly_avg})
-
-from django.db.models import Avg
-from django.db.models.functions import TruncDate
-from django.http import JsonResponse
 
 def temperature_data(request):
     # Group readings by date and calculate the daily average temperature
